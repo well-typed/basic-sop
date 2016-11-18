b42ba531317e5e5ee37ac89556d7aed9983042ae
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-11-18 11:21:05 +0200

    Improve garbitrary, add gshrink

--- src/Generics/SOP/Arbitrary.hs
+++ src/Generics/SOP/Arbitrary.hs
@@ -5,11 +5,11 @@
 --
 module Generics.SOP.Arbitrary (
     garbitrary
+  , gshrink
     -- * Re-exports
   , Arbitrary(..)
   ) where
 
-import Control.Monad
 import Test.QuickCheck
 
 import Generics.SOP
@@ -25,13 +25,41 @@ import Generics.SOP
 --
 -- > instance Arbitrary T where
 -- >   arbitrary = garbitrary
+-- >   shrink    = gshrink
 --
 -- Note that currently no attempts are being made to generate arbitrary
 -- values of a particular size, and it is possible that this function
 -- diverges for recursive structures.
 --
 garbitrary :: forall a. (Generic a, All2 Arbitrary (Code a)) => Gen a
-garbitrary = liftM to $ hsequence =<< elements (apInjs_POP $ hcpure p arbitrary)
+garbitrary = fmap to $ do
+    genSopGen <- elements (apInjs_POP $ hcpure p arbitrary) -- :: Gen (SOP Gen (Code a))
+    hsequence genSopGen
   where
     p :: Proxy Arbitrary
     p = Proxy
+
+-- | Generic shrink. See 'garbitrary'.
+--
+-- Note that this doesn't work very well for recursive structures:
+--
+-- >>> sort $ shrink ([1,2] :: [Int])
+-- [[],[0,2],[1],[1,0],[1,1],[2]]
+--
+-- >>> sort $ gshrink ([1,2] :: [Int])
+-- [[0,2],[1],[1,0],[1,1]]
+--
+gshrink :: forall a. (Generic a, All2 Arbitrary (Code a)) => a -> [a]
+gshrink
+    = map (to . SOP)
+    . hsequence'
+    . hcmap (Proxy :: Proxy (All Arbitrary)) gshrink'
+    . unSOP
+    . from
+  where
+    gshrink' :: forall ys. All Arbitrary ys => NP I ys -> ([] :.: NP I) ys
+    gshrink' Nil          = Comp $ []
+    gshrink' (I x :* Nil) = Comp $ fmap (\x' -> I x' :* Nil) $ shrink x
+    gshrink' (I x :* xs)  = Comp $
+        [ I x' :* xs  | x'  <- shrink x ] ++
+        [ I x  :* xs' | xs' <- unComp $ gshrink' xs ]
